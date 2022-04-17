7929c294b18cc610266abab19ef9de7ec25b6c70
VLSLV333 <v.sherstyk@gmail.com>
2022-04-17 13:51:16 +0000

    JSON.stringify is a powerfull tool to compare objects and arrays! It will turn them into strings, which can be then easily compared

---
+++ index.js
@@ -0,0 +1,32 @@
+// comparing if objects and arrays are equal
+
+// let v = [1, 1, 2, 3, 5, 8]// let v = {name: "Vlad", age: 25}
+// let j = [1, 1, 2, 3, 5, 8]// let j = {name: "Vlad", age: 25}
+
+// if (v === j){
+//     console.log("Equal")
+// } console.log("Different")              // will result in different, cause JS can't just compare objects and arrays  
+
+
+// let v = 7     // let v = "Vlad"
+// let j = 7             // let j = "Vlad"
+
+// if (v === j){
+//     console.log("Equal")                 // will result in Equal. Because strings and numbers can be easily compared
+// } else {
+//     console.log("Different")
+// }
+    
+// let v = [1, 1, 2, 3, 5, 8]   // let v = {name: "Vlad", age: 25}
+// let j = [1, 1, 2, 3, 5, 8]  // let j = {name: "Vlad", age: 25}
+
+// let a = JSON.stringify(v)               // makes objects and arrays a simple string!
+// let b = JSON.stringify(j)              // makes objects and arrays a simple string
+// console.log(v)
+// console.log(a)
+
+// if (a === b){
+//         console.log("Equal")                 // will result in Equal. Because strings and numbers can be easily compared
+//     } else {
+//         console.log("Different")
+//     }
